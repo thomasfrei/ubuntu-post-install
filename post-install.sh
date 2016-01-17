933fed3163fe0015a3b0be4408ec64aa190864aa
Thomas Frei <thomast.frei@gmail.com>
2016-01-17 19:48:55 +0100

    Switched theme from numix to arc Switched icon set from moka to ultra-flat-icons New Wallpaper Added composer Install LAMP stack manually instead of Tasksel Added the atom editor

--- post-install.sh
+++ post-install.sh
@@ -43,7 +43,7 @@ notify-colored() {
 echo "#-------------------------------------------#"
 echo "#                                           #"
 echo "#               Post-Install                #"
-echo "#     For Ubuntu 14.04 Trusty Tahr          #"
+echo "#     For Ubuntu 15.10 Wily Werewolf        #"
 echo "#                                           #"
 echo "#-------------------------------------------#"
 echo ""
@@ -56,32 +56,27 @@ sudo apt-get dist-upgrade -y
 echo 'Performing system update...'
 notify-colored 'Done.'
 
-# Restore Privacy
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Restoring Privacy" 
-echo 'Restoring Privacy'
-wget -q -O - https://fixubuntu.com/fixubuntu.sh | bash 
-notify-colored 'Done.'
-
 # Remove unused Software
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Uninstalling Unneeded Packages" 
+notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Uninstalling Unneeded Packages"
 echo 'Uninstalling Crap'
 sudo apt-get purge -y $(cat $UNINSTALL)
 notify-colored 'Done.'
 
 # Add PPA's
-sudo add-apt-repository ppa:numix/ppa -y
-sudo add-apt-repository ppa:moka/stable -y
+sudo add-apt-repository ppa:noobslab/themes -y
+sudo add-apt-repository ppa:noobslab/icons -y
 sudo add-apt-repository ppa:webupd8team/brackets -y
+sudo add-apt-repository ppa:webupd8team/atom -y
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 
 sudo apt-get update
 
 # Install Software
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Packages" 
+notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Packages"
 sudo apt-get install -y $(cat $INSTALL)
 notify-colored 'Done.'
 
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Google Chrome" 
+notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Google Chrome"
 if [ $(uname -i) = 'i386' ]; then
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
 elif [ $(uname -i) = 'x86_64' ]; then
@@ -90,6 +85,10 @@ fi
 sudo dpkg -i google*.deb
 rm google*.deb
 notify-colored 'Done.'
+
+notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Development Tools"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g bower
@@ -100,17 +99,10 @@ sudo rm -f -R node_modules
 
 sudo chown -R $USERNAME:www-data /var/www/
 sudo chmod -R 705 /var/www/
-
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Numix Theme" 
-sudo apt-get install -y numix-gtk-theme
 notify-colored 'Done.'
 
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Moka Icons" 
-sudo apt-get install -y moka-icon-theme
-notify-colored 'Done.'
-
-notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Solarized Terminal" 
-cd 
+notify-send -i $DIR/data/images/notify-icon.svg -t 50  "Ubuntu Post Install" "Installing Solarized Terminal"
+cd
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 sudo mv dircolors.ansi-dark .dircolors
 eval `dircolors ~/.dircolors`
@@ -123,22 +115,25 @@ sudo mv solarized_dark.xml /usr/share/gtksourceview-3.0/styles/
 notify-colored 'Done.'
 
 # Move Wallpaper to User Home
-sudo mkdir $USER_HOME/Pictures/Wallpapers
-sudo mv $DIR/data/images/wallpaper.jpg $USER_HOME/Pictures/Wallpapers
+mkdir $USER_HOME/Pictures/Wallpapers
+sudo chown -R $USERNAME:$USERNAME $USER_HOME/Pictures/Wallpapers
+mv $DIR/data/images/wallpaper.jpg $USER_HOME/Pictures/Wallpapers
+mv $DIR/data/images/wallpaper2.png $USER_HOME/Pictures/Wallpapers
 
 # Settings
-gsettings set org.gnome.desktop.interface gtk-theme "Numix Daily"
-gsettings set org.gnome.desktop.wm.preferences theme "Numix Daily"
-gsettings set org.gnome.desktop.interface icon-theme "Moka"
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Darker"
+gsettings set org.gnome.desktop.wm.preferences theme "Arc-Darker"
+gsettings set org.gnome.desktop.interface icon-theme "Ultra-Flat"
 gsettings set org.gnome.gedit.preferences.editor scheme 'solarized_dark'
 gsettings set org.gnome.gedit.preferences.editor scheme 'solarized_dark'
-gsettings set org.gnome.desktop.background picture-uri "file:///${USER_HOME}/Pictures/Wallpapers/wallpaper.jpg"
-gsettings set com.canonical.unity-greeter background "file:///${USER_HOME}/Pictures/Wallpapers/wallpaper.jpg"
 gsettings set org.gnome.desktop.interface text-scaling-factor 0.875
-org.gnome.desktop.interface cursor-size 21
+gsettings set org.gnome.desktop.interface cursor-size 21
+gsettings set org.gnome.desktop.background picture-uri "file:///${USER_HOME}/Pictures/Wallpapers/wallpaper2.png"
+gsettings set com.canonical.unity-greeter background "file:///${USER_HOME}/Pictures/Wallpapers/wallpaper2.png"
+dconf write /org/compiz/profiles/unity/plugins/unityshell/icon-size 32
 
 notify-send -i $DIR/data/images/notify-icon.svg -t 50 "Ubuntu Post Install" "Changing applications on Unity launcher..."
-gsettings set com.canonical.Unity.Launcher favorites "['application://firefox.desktop','application://google-chrome.desktop','application://brackets.desktop','application://sublime-text.desktop','application://gedit.desktop','application://filezilla.desktop','application://vlc.desktop','application://deluge.desktop','application://nautilus.desktop','application://gnome-terminal.desktop','application://google-chrome.desktop','application://unity-control-center.desktop','unity://running-apps','unity://expo-icon','unity://devices']"
+gsettings set com.canonical.Unity.Launcher favorites "['application://firefox.desktop','application://google-chrome.desktop','application://brackets.desktop','application://sublime-text.desktop', 'application://atom.desktop', 'application://gedit.desktop','application://filezilla.desktop','application://vlc.desktop','application://deluge.desktop','application://nautilus.desktop','application://gnome-terminal.desktop','application://unity-control-center.desktop','unity://running-apps','unity://expo-icon','unity://devices']"
 
 notify-send -i $DIR/data/images/notify-icon.svg -t 50 "Ubuntu Post Install" "Cleaning up.."
 sudo apt-get autoremove -y
